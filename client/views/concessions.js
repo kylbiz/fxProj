055df5ade75facfc39a1a86d41c2db6a79597c66
ZZR-china <2467254599@qq.com>
2016-06-15 18:19:10 +0800

    优惠政策

--- client/views/concessions.js
+++ client/views/concessions.js
@@ -1,24 +1,28 @@
 Template.concessions.onRendered(function () {
-	// $("#jqPaginator").jqPaginator({
-	//             totalPages: 15,
-	//             visiblePages: 10,
-	//             currentPage: 1,
-	//             first: '<a href="javascript:void(0);" class="first"> <i class="fa fa-angle-double-left" aria-hidden="true"><\/i><\/a>',
-	//             prev: '<a class="prev" href="javascript:void(0);">&lt;<\/a>',
-	//             next: '<a class="next" href="javascript:void(0);">&gt;<\/a>',
-	//             page: '<a href="javascript:void(0);">{{page}}<\/a>',
-	//             last: '<a href="javascript:void(0);" class="last"><i class="fa fa-angle-double-right" aria-hidden="true"><\/i><\/a>',
-	//             onPageChange: function (n) {
-	               
-	//             }
-	// });
+	$('.ul-text ul li:nth-child(2)').addClass('normal-red');
+	$('.ul-text ul li:nth-child(2)').find('input').attr('checked','checked');
+	$('#showUltext1').css('display','none');
+	$('#showUltext2').css('display','none');
 })
 Template.concessions.events({
 	'click .ul-text ul li': function (event) {
 		// ...
-		$(event.target).toggleClass('normal-red');
-
+		var target = event.target;
+		if ( target != $('.ul-text ul li:nth-child(2)')) {
+			$('.ul-text ul li:nth-child(2)').find('input').removeAttr('checked').removeClass('normal-red');
+			$(target).find('input').click();
+		}else{
+			$('.ul-text ul li:nth-child(2)').find('input').attr('checked','checked');
+		}
+		$(target).toggleClass('normal-red');
+	},
+	'click #upSpan1': function (event) {
+			$('#showUltext1').toggleClass('Dblock').prev().toggleClass('spanChange');
+	},
+	'click #upSpan2': function (event) {
+			$('#showUltext2').toggleClass('Dblock1').prev().toggleClass('spanChange');
 	}
+
 });
 
 
