7067d726c0c81b25ce1ee717b308a07ee911765a
ZZR-china <2467254599@qq.com>
2016-07-01 15:45:02 +0800

    map change

--- client/views/contactUs.js
+++ client/views/contactUs.js
@@ -1,3 +1,7 @@
+Template.map.onRendered(function () {
+
+})
+
 Template.map.events({
 	// 'click .tabs li': function (e) {
 	// 	//四个标签切换
@@ -15,5 +19,28 @@ Template.map.events({
 		var index = $(" .transport li").index(target);
 		$(target).addClass('map-active').siblings().removeClass('map-active');
 		$('.map-info div').eq(index).addClass('show').siblings().removeClass('show');
-	}	
+	},
+	'click .item': function (e) {
+		var target = $(e.currentTarget),
+			val = target.attr('val'),
+			selfRoute = $('.self-route');
+		var selfDiv = $('.self-route div');
+		selfRoute.css('cssText','background:url(/images/contactus/selfdrive/'+val+'.jpg) no-repeat');
+		$('.all-routes').find('.item').removeClass('bggray');
+		target.addClass('bggray');
+		console.log(selfDiv[0]);
+		for (var i = 0; i <6; i++) {
+			if ($(selfDiv[i]).attr('val') == val) {
+				$(selfDiv[i]).css('display','none').siblings().css('display','block');
+			}
+		}
+		
+	},
+	'click .self-route div': function (e) {
+		var target = $(e.currentTarget),
+			val = target.attr('val'),
+			selfRoute = $('.self-route');
+		selfRoute.css('cssText','background:url(/images/contactus/selfdrive/'+val+'.jpg) no-repeat');
+		target.css('display','none').siblings().css('display','block');
+	}
 });
