69d838c9736b07d40807a8d54fe63ed35d5d7a40
arronf2e <191446367@qq.com>
2016-07-28 19:00:37 +0800

    update

--- client/views/fxAdvantages.js
+++ client/views/fxAdvantages.js
@@ -1,40 +1,44 @@
-Template.fxAdvantages.onRendered(function () {
-	   
+Template.fxAdvantages.onRendered(function() {
+
 })
 
 Template.fxAdvantages.helpers({
-	isClick: function () {
+	isClick: function() {
 		// ...
 		return Session.set('isClick', false);
 	},
-	isHover: function () {
+	isHover: function() {
 		return Session.set('isHover', false);
 	},
 });
 
 Template.fxAdvantages.events({
-	'click .pictures div': function (e) {
+	'click .pictures div': function(e) {
 		// ...
 		// var attr = $('.pictrues div').attr('class');
-		var Imgs= new Array("img7","img1","img2","img3","img4","img5","img6");
+		var Imgs = new Array("img7", "img1", "img2", "img3", "img4", "img5", "img6");
 		console.log(Imgs[0]);
 		var target = e.target;
+		var index = $(".pictures div").index(target);
 		var newUrl = ($(target).attr("newUrl"));
 		var contentDetail = ($(target).attr("data-next"));
 		var is = ($(target).attr("is"));
-		var divcss = {background:'url(images/advantages/'+targetClass+'_click.png)'};
+		var divcss = {
+			background: 'url(images/advantages/' + targetClass + '_click.png)'
+		};
 		var targetClass = $(target).attr('class');
-		$('.img7').css("cssText","background:url(images/advantages/img7.png) no-repeat");
-		$('.img1').css("cssText","background:url(images/advantages/img1.png) no-repeat");
-		$('.img2').css("cssText","background:url(images/advantages/img2.png) no-repeat");
-		$('.img3').css("cssText","background:url(images/advantages/img3.png) no-repeat");
-		$('.img4').css("cssText","background:url(images/advantages/img4.png) no-repeat");
-		$('.img5').css("cssText","background:url(images/advantages/img5.png) no-repeat");
-		$('.img6').css("cssText","background:url(images/advantages/img6.png) no-repeat");
-		$(target).attr('is',true);
-		$(target).css('background-image','url('+newUrl+')');
-		$('#'+contentDetail).show().siblings().hide();
-		return Session.set('isHover', true),Session.set('isClick', true);
+		$('.img7').css("cssText", "background:url(images/advantages/img7.png) no-repeat");
+		$('.img1').css("cssText", "background:url(images/advantages/img1.png) no-repeat");
+		$('.img2').css("cssText", "background:url(images/advantages/img2.png) no-repeat");
+		$('.img3').css("cssText", "background:url(images/advantages/img3.png) no-repeat");
+		$('.img4').css("cssText", "background:url(images/advantages/img4.png) no-repeat");
+		$('.img5').css("cssText", "background:url(images/advantages/img5.png) no-repeat");
+		$('.img6').css("cssText", "background:url(images/advantages/img6.png) no-repeat");
+		$(target).attr('is', true);
+		$(target).css('background-image', 'url(' + newUrl + ')');
+		// $('#' + contentDetail).show().siblings().hide();
+		$(".content-detail .cc").eq(index).show().siblings().hide();
+		return Session.set('isHover', true), Session.set('isClick', true);
 	},
 	// 'mouseover .pictures div': function (e) {
 	// 	var tab = e.currentTarget;
